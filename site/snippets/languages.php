068d29f0dd9b5c2e3c631a5c02a3de7c7e3e971a
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-01 15:47:10 +0200

    Added buttons & lang toggle

--- site/snippets/languages.php
+++ site/snippets/languages.php
@@ -1,7 +1,7 @@
 <div class="header-languages">
-  <div class="btn-group">
-    <button type="button" class="button button--dropdown"><?= $site->language()->name() ?></button>
-    <ul class="button-dropdown">
+  <div class="dropdown">
+    <button type="button" class="button button button--color-2 button--s button--outline button--toggle"><?= $site->language()->name() ?><span class="button-caret"></span></button>
+    <ul class="dropdown-menu">
       <?php foreach ( $site->languages() as $language ) : ?>
         <li class="language<?php e( $site->language() == $language, ' language--active' ) ?>">
           <a href="<?= $language->url() ?>" rel="bookmark"><?= html( $language->name() ) ?></a>
