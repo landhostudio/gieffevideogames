365e4d7d5fd8feef444a4b49668e403137d30b3d
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-01 15:47:29 +0200

    Added lang toogle js

--- assets-src/js/main.js
+++ assets-src/js/main.js
@@ -3,7 +3,19 @@
   'use strict';
 
   var init = function() {
+    initLanguage();
     initLog();
+  };
+
+  function initLanguage() {
+    $('.button--toggle').click(function() {
+      $('.dropdown').toggleClass('dropdown--active');
+    });
+    $(document).click(function(event) {
+      if (event.target.nodeName.toLowerCase() != 'button') {
+        $('.dropdown').removeClass('dropdown--active');
+      }
+    });
   };
 
   function initLog() {
