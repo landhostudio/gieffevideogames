4581fe6811f6271c700259a7e369ae0900efcb4b
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-04 22:13:50 +0200

    Fixed menu border-top color issue

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,6 +1,6 @@
 <nav role="navigation" class="header-menu">
   <h2 class="hidden"><?= l( 'menu.title' ) ?></h2>
-  <ul class="header-menu-items">
+  <ul class="header-menu-items<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php else : ?> text-color-2<?php endif; ?>">
     <?php foreach ( $pages->visible() as $item ) : ?>
       <li class="header-menu-item">
         <a href="<?= $item->url() ?>" class="menu-link<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php if ( $item->isOpen() ) : ?> text-color-4<?php else : ?> text-color-4-alt<?php endif; ?><?php else : ?><?php if ( $item->isOpen() ) : ?> text-color-2<?php else : ?> text-color-3<?php endif; ?><?php endif; ?>"><?= $item->title()->html() ?></a>
