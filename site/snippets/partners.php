2524896a3a4a75cccc669c34575a26281bb041ff
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-05 15:45:35 +0200

    Added partners

--- site/snippets/partners.php
+++ site/snippets/partners.php
@@ -1,16 +1,18 @@
 <?php if ( $site->partners() ) : ?>
   <section class="partners">
-    <h3><?= l( 'partners.title' ) ?></h3>
-    <ul>
-      <?php foreach ( $site->partners()->toStructure() as $partner ) : ?>
-        <?php if ( $image = $site->image( $partner->image() ) ) : ?>
-          <li>
-            <a href="<?= $partner->url(); ?>" rel="nofollow" target="_blank">
-              <?= thumb( $image, array( 'height' => 80 ) ); ?>
-            </a>
-          </li>
-        <?php endif; ?>
-      <?php  endforeach; ?>
-    </ul>
+    <div class="container">
+      <h3 class="h4"><?= l( 'partners.title' ) ?></h3>
+      <ul class="partners-items no-list inline-list">
+        <?php foreach ( $site->partners()->toStructure() as $partner ) : ?>
+          <?php if ( $image = $site->image( $partner->image() ) ) : ?>
+            <li class="partners-item">
+              <a href="<?= $partner->url(); ?>" rel="nofollow" target="_blank" class="partners-item-link">
+                <?= thumb( $image, array( 'height' => 80 ) ); ?>
+              </a>
+            </li>
+          <?php endif; ?>
+        <?php  endforeach; ?>
+      </ul>
+    </div>
   </section>
 <?php endif; ?>
