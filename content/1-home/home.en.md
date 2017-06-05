64419f639442a770c69e1ebfdf800cb4aa5738eb
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-05 16:41:29 +0200

    Added sections

--- content/1-home/home.en.md
+++ content/1-home/home.en.md
@@ -43,18 +43,30 @@ Sections:
 - 
   image: home-section-1.jpg
   title: Virtual Reality
-  description: This is VR section.
+  description: >
+    At half-past eight the door opened, the
+    policeman appeared, and, requesting them
+    to follow him, led the way to an
+    adjoining hall.
   ctatitle: Read more
   ctaurl: virtual-reality
 - 
   image: home-section-2.jpg
   title: Products
-  description: This is products section
+  description: >
+    At half-past eight the door opened, the
+    policeman appeared, and, requesting them
+    to follow him, led the way to an
+    adjoining hall.
   ctatitle: Our products
   ctaurl: products
 - 
   image: home-section-3.jpg
   title: Gaming Hall
-  description: This is gaming hall section, yo!
+  description: >
+    At half-past eight the door opened, the
+    policeman appeared, and, requesting them
+    to follow him, led the way to an
+    adjoining hall.
   ctatitle: Read more
   ctaurl: gaming-hall
--- site/snippets/sections.php
+++ site/snippets/sections.php
@@ -1,21 +1,24 @@
 <?php if ( $page->sections() ) : ?>
   <?php $n = 0; foreach ( $page->sections()->toStructure() as $section ) : $n++; ?>
     <section class="section<?= ( $n%2 ) ? ' section--right' : ' section--left' ?>">
-      
-      <?php if ( $image = $page->image( $section->image() ) ) : ?>
-        <div class="section-image">
-          <?= thumb( $image, array( 'width' => 300, 'crop' => true ) ); ?>
+      <div class="container-full">
+        <div class="section-container">
+          <?php if ( $image = $page->image( $section->image() ) ) : ?>
+            <div class="section-image">
+              <div class="section-image-container">
+                <?= thumb( $image, array( 'width' => 960, 'height' => 540, 'crop' => true ) ) ?>
+              </div>
+            </div>
+          <?php endif; ?>
+          <div class="section-content">
+            <h3 class="h2<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php else : ?> text-color-3<?php endif; ?>"><?= $section->title(); ?></h3>
+            <p><?= $section->description(); ?></p>
+            <?php if ( $section->ctatitle()->isNotEmpty() && $section->ctaurl()->isNotEmpty() ) : ?>
+              <a href="<?= $site->language()->url() ?>/<?= $section->ctaurl(); ?>" rel="bookmark" class="button<?php if ( $page->template() == 'virtual-reality' ) : ?> button--color-4<?php else : ?> button--color-2<?php endif; ?> button--m button--outline"><?= $section->ctatitle(); ?></a>
+            <?php endif; ?>
+          </div>
         </div>
-      <?php endif; ?>
-
-      <div class="section-content">
-        <h3><?= $section->title(); ?></h3>
-        <p><?= $section->description(); ?></p>
-        <?php if ( $section->ctatitle()->isNotEmpty() && $section->ctaurl()->isNotEmpty() ) : ?>
-          <a href="<?= $site->language()->url() ?>/<?= $section->ctaurl(); ?>" rel="bookmark" class="button"><?= $section->ctatitle(); ?></a>
-        <?php endif; ?>
       </div>
-
     </section>
   <?php endforeach; ?>
 <?php endif; ?>
