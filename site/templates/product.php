f918953a395083fa0333d0ef73aabaa4c1774ffc
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-06 15:45:47 +0200

    Added product spects

--- site/templates/product.php
+++ site/templates/product.php
@@ -8,15 +8,17 @@
 
   <?php if ( $page->specs() ) : ?>
     <section class="specs">
-      <h3 class="hidden"><?= l( 'product.specs' ) ?></h3>
-      <dl>
-        <?php foreach ( $page->specs()->yaml() as $spec ) : ?>
-          <dt class="specs-header"><?= $spec[ 'title' ] ?></dt>
-          <dd class="specs-description">
-            <?= kirbytext( $spec[ 'description' ] ) ?>
-          </dd>
-        <?php endforeach; ?>
-      </dl>
+      <div class="container">
+        <h3 class="specs-title h2 text-color-3"><?= l( 'product.specs' ) ?></h3>
+        <dl>
+          <?php foreach ( $page->specs()->yaml() as $spec ) : ?>
+            <dt class="specs-header"><?= $spec[ 'title' ] ?></dt>
+            <dd class="specs-description">
+              <?= kirbytext( $spec[ 'description' ] ) ?>
+            </dd>
+          <?php endforeach; ?>
+        </dl>
+      </div>
     </section>
   <?php endif; ?>
 
