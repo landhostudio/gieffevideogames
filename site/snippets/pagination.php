23033a67f899a32c5fd97a45c7b218a9d8584feb
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-06 14:17:36 +0200

    Improved product pagination

--- site/snippets/pagination.php
+++ site/snippets/pagination.php
@@ -1,32 +1,46 @@
 <?php if ( $pagination->hasPages() ) : ?>
-  <nav class="pagination">
-    <h4 class="hidden"><?= l( 'pagination.title' ) ?></h4>
-    <ul>
-      <?php if ( $pagination->hasPrevPage() ) : ?>
-        <li class="pagination-prev">
-          <a href="<?= $pagination->prevPageURL() ?>" rel="prev"><?= l( 'pagination.prev' ) ?></a>
-        </li>
-      <?php else : ?>
-        <li class="pagination-prev pagination-prev--disabled">
-          <span><?= l( 'pagination.prev' ) ?></span>
-        </li>
-      <?php endif ?>
+  <nav class="pagination<?php if ( $position == 'top' ) : ?> pagination--top<?php else: ?> pagination--bottom<?php endif; ?>">
+    <div class="container">
 
-      <?php foreach ( $pagination->range(6) as $page ) : ?>
-        <li class="pagination-page<?php if ( $pagination->page() == $page ) echo ' pagination-page--active' ?>">
-          <a href="<?= $pagination->pageURL( $page ) ?>"><?= $page ?></a>
-        </li>
-      <?php endforeach ?>
+      <h4 class="hidden"><?= l( 'pagination.title' ) ?></h4>
 
-      <?php if ( $pagination->hasNextPage() ) : ?>
-        <li class="pagination-next">
-          <a href="<?= $pagination->nextPageURL() ?>" rel="next"><?= l( 'pagination.next' ) ?></a>
-        </li>
-      <?php else : ?>
-        <li class="pagination-next pagination-next--disabled">
-          <span><?= l( 'pagination.next' ) ?></span>
-        </li>
-      <?php endif ?>
-    </ul>
+      <div class="pagination-content">
+        <?php if ( $pagination->hasPrevPage() ) : ?>
+          <div class="pagination-prev">
+            <a href="<?= $pagination->prevPageURL() ?>" rel="prev">
+              <span><?= l( 'pagination.prev' ) ?></span>
+              <?= ( new Asset( "assets/images/arrow-left.svg" ) )->content() ?>
+            </a>
+          </div>
+        <?php else : ?>
+          <div class="pagination-prev pagination-prev--disabled">
+            <span><?= l( 'pagination.prev' ) ?></span>
+            <?= ( new Asset( "assets/images/arrow-left.svg" ) )->content() ?>
+          </div>
+        <?php endif ?>
+
+        <?php if ( $pagination->hasNextPage() ) : ?>
+          <div class="pagination-next">
+            <a href="<?= $pagination->nextPageURL() ?>" rel="next">
+              <span><?= l( 'pagination.next' ) ?></span>
+              <?= ( new Asset( "assets/images/arrow-right.svg" ) )->content() ?>
+            </a>
+          </div>
+        <?php else : ?>
+          <div class="pagination-next pagination-next--disabled">
+            <span><?= l( 'pagination.next' ) ?></span>
+            <?= ( new Asset( "assets/images/arrow-right.svg" ) )->content() ?>
+          </div>
+        <?php endif ?>
+        <ul class="pagination-pages no-list inline-list">
+          <?php foreach ( $pagination->range(4) as $page ) : ?>
+            <li class="pagination-page<?php if ( $pagination->page() == $page ) echo ' pagination-page--active' ?>">
+              <a href="<?= $pagination->pageURL( $page ) ?>"><?= $page ?></a>
+            </li>
+          <?php endforeach ?>
+        </ul>
+      </div>
+
+    </div>
   </nav>
 <?php endif ?>
