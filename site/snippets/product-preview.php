3f0fc793b0507f34936d5bff8b6162a0e13a98c7
h0r0man <roman.horokhovatskyy@gmail.com>
2017-06-06 18:37:27 +0200

    Added related/featured products

--- site/snippets/product-preview.php
+++ site/snippets/product-preview.php
@@ -2,16 +2,16 @@
   <a href="<?= $product->url() ?>" rel="bookmark" class="products-preview-link">
 
     <?php if ( $image = $product->heroImage()->toFile() ) : ?>
-      <div class="products-preview-image">
+      <div class="products-preview-image<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php else : ?> text-color-3<?php endif; ?>">
         <?= thumb( $image, array( 'width' => 720, 'height' => 720, 'crop' => true ) ) ?>
       </div>
     <?php endif; ?>
 
     <div class="products-preview-content">
       <?php if ( $product->heroTitle() ): ?>
-        <h3 class="text-color-2"><?= $product->heroTitle()->html() ?></h3>
+        <h3 class="<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php else : ?> text-color-2<?php endif; ?>"><?= $product->heroTitle()->html() ?></h3>
       <?php else : ?>
-        <h3 class="text-color-2"><?= $product->title()->html() ?></h3>
+        <h3 class="<?php if ( $page->template() == 'virtual-reality' ) : ?> text-color-4<?php else : ?> text-color-2<?php endif; ?>"><?= $product->title()->html() ?></h3>
       <?php endif; ?>
     </div>
   </a>
